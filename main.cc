8d6dc04524aa23f3bcab08be4b281baba0f28036
Jordan Zenisek <jordanzenisek@Jordans-MacBook-Pro-97.local>
2017-12-12 11:24:47 -0500

    working on the code in the main and created a text file for testing

--- main.cc
+++ main.cc
@@ -1,16 +1,61 @@
 #include<iostream>
+#include<fstream>
 
 using namespace std;
 
+void countLine(char* pName);
+void countChar(char* pName);
+
 int main(){
+	char* pName;
 
+	cout<<"Please enter the file you would like to have counted or type No.\n";
+	cin>>pName;
 
+//	if(pName == 'No' || pName == 'no'){
+//		countLine('Ohio University');
+//		countChar('Athens');
+//	}else{
+		countLine(pName);
+		countChar(pName);
+//	}		
+
+	return 0;
 }
 
-int countLine(char pName){
+void countLine(char* pName){
 
+	int num; //number of lines
+	string line;
+
+	ifstream myfile(pName);
+
+	if(myfile.is_open()){
+
+		while(!myfile.eof()){
+			getline(myfile, line);
+			num++;
+		}
+	}
+	cout<<num<<" Lines\n";
 }
 
-int countChar(char pName){
+void countChar(char* pName){
+	int num; //number of characters
+	char c;
 
+	ifstream myfile(pName);
+	
+	if(myfile.is_open()){
+		while(!myfile.eof()){
+//			if(fin.peek() == -1)
+//				break;
+
+			c = fin.get();
+
+			if(c != '\n')
+				num++;
+		}
+	}
+	cout<<num<<" Characters\n";
 }
